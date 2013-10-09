000c8ebeae4fb30ff5802d4497b5a34997f00f14
Michael Wenger <michaelwenger27@gmail.com>
2013-10-08 20:52:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retflix::Application.config.secret_key_base = '51263fd0868d5fdbcac2fcbe1a11be199bf13347cd403e9ca76ba58ab0a4304d03ec5c77c2f48751d62036ec7b3d6f1fe2de688718c9ebb16825b894a174d686'
